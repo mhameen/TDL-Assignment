a90a0b045c4086b3f6d56dc84f42f71f2a09d10e
mhameen <mhameen001@gmail.com>
2023-04-12 00:09:59 +0530

    done to-do-list Assignment

---
+++ script.js
@@ -0,0 +1,19 @@
+console.log("Hello Ameen")
+
+
+
+function addElement(){
+    let toDoContainer = document.getElementById("to-do-container")
+    let newTag = document.createElement("p")
+    let inputText = document.getElementById("input-text");
+    let text = inputText.value
+    newTag.innerText = text ;
+    let removeText = newTag.setAttribute("onclick","removeItem(this)")
+
+    toDoContainer.appendChild(newTag);
+}
+
+function removeItem(hello){
+    let toDoContainer = document.getElementById("to-do-container")
+    toDoContainer.removeChild(hello)
+}
